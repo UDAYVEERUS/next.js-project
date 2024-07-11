7f0a8c756292bcb0ff6514599f8015870c191747
UDAYVEERUS <udayveerus348567@gmail.com>
2024-07-11 21:20:24 +0530

    create components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,14 @@ import Products from "@/components/Products";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Gallery from "@/components/Gallery";
+import Form from "@/components/Form";
 export default function Home() {
   return (
    <div>
       <Header />
       <Products />
       <Gallery />
+      <Form/>
       <Footer />
    </div>
   );

0768934a3be411d38967391eef4990e135a989b9
UDAYVEERUS <udayveerus348567@gmail.com>
2024-07-13 00:38:26 +0530

    Add cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,9 @@ import Products from "@/components/Products";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Gallery from "@/components/Gallery";
+// import Slider from "@/components/Slider";
+import Popupmodal from "@/components/Popup-modal";
+import Cards from "@/components/Cards";
 
 export default function Home() {
   return (
@@ -11,7 +14,10 @@ export default function Home() {
       <Header />
       <Products />
       <Gallery />
+      <Cards />
       {/* <Form/> */}
+      {/* <Slider/> */}
+      <Popupmodal />
       <Footer />
    </div>
   );
